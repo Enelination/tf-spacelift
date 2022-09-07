cbca6cdb33d058b97217bc425bc2df40ccba4315
enel <enel@gmail.com>
2022-09-07 13:22:03 +0000

    ...

--- providers.tf
+++ providers.tf
@@ -9,5 +9,5 @@ terraform {
 provider "aws" {
   region                  = var.aws_region
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "Renmo"
+  profile                 = "user-profie"
 }
